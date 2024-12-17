25144e63d33ffa973bdc502e0b2d32a7e9287cfe
alexandre <alexandrehiance@gmail.com>
2024-12-17 10:39:42 +0100

    [feat] : Monorepo quest 3

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,7 +8,6 @@ import "../database/checkConnection";
 
 // Import the Express application from ./app
 import app from "./app";
-import type { RequestHandler } from "express";
 
 // Get the port from the environment variables
 const port = process.env.APP_PORT;
@@ -21,10 +20,3 @@ app
   .on("error", (err: Error) => {
     console.error("Error:", err.message);
   });
-
-
-  const sayWelcome: RequestHandler = (req, res) => {
-    res.send("Welcome to Wild Series !");
-  };
-
-  app.get("/", sayWelcome);
